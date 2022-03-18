8d40da1258a3841f20786b3eaff23174ae7a1567
ADmin <ADmin@DESKTOP-QOPCTRT>
2022-03-18 22:32:45 +0530

    Second Commit:TestCase2 Created

--- GITHUB/src/githubdemo/Testcase2.java
+++ GITHUB/src/githubdemo/Testcase2.java
@@ -1,5 +1,25 @@
 package githubdemo;
 
-public class Testcase2 {
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import orangehrmlibrary.Employee;
+import orangehrmlibrary.LoginPage;
+
+public class Testcase2 
+{
+
+	@Parameters({"fname","lname"})
+	@Test
+	public void checkemployeereg(String fname,String lname) throws InterruptedException
+	{
+		LoginPage lp=new LoginPage();
+		lp.login("Admin", "Qedge123!@#");
+		
+		Employee emp=new Employee ();
+		boolean res=emp.addemployee(fname, lname);
+		Assert.assertTrue(res);
+		lp.logout();
 
 }
